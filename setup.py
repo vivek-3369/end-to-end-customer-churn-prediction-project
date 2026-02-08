658c05f4b425e648bcb58cfca12b2401ea5e2e69
vivek-3369 <boddulvivek747@gmail.com>
2026-02-08 15:49:42 +0530

    updating setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+from typing import List 
+
+def get_requirements(file_path:str)->List[str]:
+    
+    with open(file_path,'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        return requirements
+    
+setup(
+    name="customer-churn-prediction-model",
+    version="0.0.1",
+    author="Vivek Boddul",
+    author_email="boddulvivek7474@gmail.com",
+    packages=find_packages(),
+    install_packages=get_requirements('requirements.txt')
+)
