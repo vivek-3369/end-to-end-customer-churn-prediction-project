716ddaade96cea7d9929358c85a0162413735526
vivek-3369 <boddulvivek7474@gmail.com>
2026-03-14 14:38:05 +0530

    training pipeline execution

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-from src.exception import CustomException
-from src.logger import logging
-from src.components.data_ingestion import DataIngestion,DataIngestionConfig
-from src.components.data_transformation import DataTransformation,DataTransformationConfig
-
 import sys 
-import warnings
-warnings.filterwarnings("ignore")
+from src.logger import logging
+from src.exception import CustomException
+
+from src.pipelines.training_pipeline import TrainingPipeline
+
 
 if __name__ == "__main__" :
-    
-    logging.info("The execution has started")
 
-    try:
-        data_ingestion_config = DataIngestionConfig()
-        data_ingestion = DataIngestion()
-        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+    try :
+        logging.info("Training Pipeline Started")
 
-
-        data_transofrmation_config = DataTransformationConfig()
-        data_transformation = DataTransformation()
-        train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        training_pipeline = TrainingPipeline()
+        training_pipeline.start_training_pipeline()
 
     except Exception as e:
         raise CustomException(e, sys)
